c5ed08dc9f2422b6144db94f57716fae54433af2
t-kabu <t.kabu.03@gmail.com>
2022-07-23 11:30:42 +0900

    chore: windi.cssの読み込みエラーを回避

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import { CookiesProvider } from 'react-cookie'
 import { SWRConfig } from 'swr'
 import { useObserveAuthInfoExpired } from 'hooks/login/useIsLoggedIn'
+// eslint-disable-next-line import/no-unresolved
 import 'windi.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
