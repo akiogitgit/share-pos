ada9b73e30a07910463f62eaf662627bd6be82e8
Akio Tanaka <akimaron2828@icloud.com>
2022-08-30 16:03:29 +0900

    feat: そのページに行ったときにメニューを目立つように

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,17 +1,22 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { FC } from 'react'
 import { AiTwotoneHome, AiOutlineSearch, AiOutlineUser } from 'react-icons/ai'
 
 export const Footer: FC = () => {
+  const router = useRouter()
+
   return (
     <footer className='sm:hidden'>
-      <button className='border rounded-full font-bold bg-red-500 border-red-500 text-white p-2 right-20px bottom-50px text-30px fixed'>
-        <Link href='/create'>＋</Link>
-      </button>
+      {router.pathname !== '/create' && (
+        <button className='border rounded-full font-bold bg-red-500 border-red-500 text-white p-2 right-20px bottom-50px text-30px fixed'>
+          <Link href='/create'>＋</Link>
+        </button>
+      )}
 
       <nav className='bg-white flex w-full py-1.5 bottom-0 left-0 text-25px justify-around fixed'>
         <Link href='/'>
-          <div>
+          <div className={`${router.pathname === '/' && 'text-red-500'}`}>
             <AiTwotoneHome className='cursor-pointer' />
           </div>
         </Link>
@@ -22,7 +27,11 @@ export const Footer: FC = () => {
           </div>
         </Link>
         <Link href='/myPage'>
-          <div>
+          <div
+            className={`${
+              router.pathname.includes('/myPage') && 'text-red-500'
+            }`}
+          >
             <AiOutlineUser className='cursor-pointer' />
           </div>
         </Link>
