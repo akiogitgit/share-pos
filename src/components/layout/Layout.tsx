ca8da8925bedac98178f0fc0ca99031b6a435f99
Akio Tanaka <akimaron2828@icloud.com>
2022-08-30 14:41:40 +0900

    fix: ヘッダーとメインの間を調整

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -9,8 +9,10 @@ export const Layout: FC<Props> = ({ children }) => {
   return (
     <>
       <Header />
-      <main className='bg-red-100 min-h-100vh pt-130px overflow-hidden '>
-        <div className='mx-auto max-w-1110px px-2 break-words'>{children}</div>
+      <main className='bg-red-100 min-h-100vh  overflow-hidden '>
+        <div className='mx-auto mt-100px max-w-1110px px-2 break-words sm:mt-130px'>
+          {children}
+        </div>
       </main>
     </>
   )
