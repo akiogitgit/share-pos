53e131abfffa7c984c194773868df4e3a369f02b
Akio Tanaka <akimaron2828@icloud.com>
2022-08-31 09:07:21 +0900

    fix: mainのmargin調整

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -11,7 +11,7 @@ export const Layout: FC<Props> = ({ children }) => {
     <>
       <Header />
       <main className='bg-red-100 min-h-100vh overflow-hidden'>
-        <div className='mx-auto mt-100px max-w-1110px px-2 break-words sm:mt-130px'>
+        <div className='mx-auto mt-100px mb-70px max-w-1110px px-2 break-words sm:mt-130px'>
           {children}
         </div>
       </main>
