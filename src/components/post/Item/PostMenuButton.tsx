be7d48546816ddf348f09c9c13ba2c4d3a2ccd05
akiogitgit <akimaron2828@icloud.com>
2022-09-13 13:32:54 +0900

    fix: Post menu の場所修正

--- src/components/post/Item/PostMenuButton.tsx
+++ src/components/post/Item/PostMenuButton.tsx
@@ -50,64 +50,70 @@ export const PostMenuButton: FC<Props> = ({
             onClick={closeMenu}
             className='h-100vh top-0 left-0 w-100vw z-10 fixed'
           ></div>
-          <div className='border cursor-pointer bg-red-100 border-red-600 rounded-10px shadow-lg transform top-[-35px] right-[-27px] shadow-red-200 w-170px z-11 absolute sm:top-[-30px] sm:right-5px sm:w-150px'>
-            {cookies.userInfo?.id === post.userId && (
-              <>
-                <div
-                  className='rounded-t-10px px-4 pt-2 pb-1 hover:bg-red-300'
-                  onClick={() => {
-                    onEdit?.()
-                    setIsOpenMenu(false)
-                  }}
-                >
-                  投稿を編集する
-                </div>
-                <div
-                  className='py-1 px-4 hover:bg-red-300'
-                  onClick={async () => {
-                    closeMenu()
-                    await deletePost()
-                  }}
-                >
-                  投稿を
-                  <span className='font-bold text-red-500 text-18px'>削除</span>
-                  する
-                </div>
-              </>
-            )}
-            <div
-              className={`py-1 px-4 hover:bg-red-300 ${
-                cookies.userInfo?.id !== post.userId && 'pt-2 rounded-t-10px'
-              }`}
-              onClick={() => {
-                navigator.clipboard.writeText(post.url)
-                alert('リンクをコピーしました')
-                closeMenu()
-              }}
-            >
-              記事リンクをコピー
-            </div>
-            {cookies.userInfo && (
-              <div className='rounded-b-10px group relative'>
-                <div
-                  className='px-4 pt-1 pb-2 hover:bg-red-300'
-                  onClick={() => setIsOpenFolder(!isOpenFolder)}
-                >
-                  ブックマークに追加
-                </div>
 
-                {/* 自分のフォルダ一覧  */}
-                <div
-                  className={`${
-                    isOpenFolder ? '' : 'hidden'
-                  } sm:group-hover:block`}
-                >
-                  <div className='top-0px right-150px absolute sm:right-80px'>
-                    <FolderList post={post} onClickFolderName={closeMenu} />
+          <div className='top-[-35px] right-5px z-11 absolute sm:top-[-35px] '>
+            <div className='border cursor-pointer bg-red-100 border-red-600 rounded-10px shadow-lg transform shadow-red-200 w-170px sm:w-150px'>
+              {/* <div className='border cursor-pointer bg-red-100 border-red-600 rounded-10px shadow-lg transform top-[-35px] right-[-27px] shadow-red-200 w-170px z-11 absolute sm:top-[-30px] sm:right-5px sm:w-150px'> */}
+              {cookies.userInfo?.id === post.userId && (
+                <>
+                  <div
+                    className='rounded-t-10px px-4 pt-2 pb-1 hover:bg-red-300'
+                    onClick={() => {
+                      onEdit?.()
+                      setIsOpenMenu(false)
+                    }}
+                  >
+                    投稿を編集する
+                  </div>
+                  <div
+                    className='py-1 px-4 hover:bg-red-300'
+                    onClick={async () => {
+                      closeMenu()
+                      await deletePost()
+                    }}
+                  >
+                    投稿を
+                    <span className='font-bold text-red-500 text-18px'>
+                      削除
+                    </span>
+                    する
+                  </div>
+                </>
+              )}
+              <div
+                className={`py-1 px-4 hover:bg-red-300 ${
+                  cookies.userInfo?.id !== post.userId && 'pt-2 rounded-t-10px'
+                }`}
+                onClick={() => {
+                  navigator.clipboard.writeText(post.url)
+                  alert('リンクをコピーしました')
+                  closeMenu()
+                }}
+              >
+                記事リンクをコピー
+              </div>
+              {cookies.userInfo && (
+                <div className='rounded-b-10px group relative'>
+                  <div
+                    className='px-4 pt-1 pb-2 hover:bg-red-300'
+                    onClick={() => setIsOpenFolder(!isOpenFolder)}
+                  >
+                    ブックマークに追加
+                  </div>
+
+                  {/* 自分のフォルダ一覧  */}
+                  <div
+                    className={`${
+                      isOpenFolder ? '' : 'hidden'
+                    } sm:group-hover:block`}
+                  >
+                    <div className='top-0px right-150px absolute sm:right-80px'>
+                      <FolderList post={post} onClickFolderName={closeMenu} />
+                    </div>
                   </div>
                 </div>
-              </div>
-            )}
+              )}
+            </div>
           </div>
         </div>
       )}
