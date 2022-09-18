191e2cc0f65aceeb2f473b95127a0f59e63536a0
akiogitgit <akimaron2828@icloud.com>
2022-09-18 09:42:15 +0900

    faet: Postの追加・削除時に、myPostsのmutateもする

--- src/hooks/usePost.ts
+++ src/hooks/usePost.ts
@@ -6,25 +6,35 @@ import { User } from 'types/user/user'
 import { deleteApi, HttpError, postApi, putApi } from 'utils/api'
 
 export const useCreatePost = () => {
-  const { data: posts, mutate } = useGetApi<Post[]>('/posts')
+  const { data: user } = useGetApi<User>('/users/me')
+  const { data: posts, mutate: mutatePosts } = useGetApi<Post[]>('/posts')
+  const { data: myPosts, mutate: mutateMyPosts } = useGetApi<MyPosts>(
+    `/users/${user?.id}`,
+  )
   const router = useRouter()
 
   const createPost = useCallback(
     async (params: PostRequestParams) => {
       try {
         const newPost = await postApi<Post>('/posts', params)
-        if (newPost && posts) {
-          console.log('投稿の作成に成功 ', newPost)
-          router.push('/')
-          mutate([...posts, newPost], false)
+        if (!newPost || !posts || !myPosts) {
+          return
         }
+
+        mutatePosts([...posts, newPost], false)
+        mutateMyPosts(
+          { user: myPosts.user, posts: [...myPosts.posts, newPost] },
+          false,
+        )
+        console.log('投稿の作成に成功 ', newPost)
+        router.push('/')
       } catch (e) {
         if (e instanceof HttpError) {
           console.error(e.message)
         }
       }
     },
-    [mutate, posts, router],
+    [mutateMyPosts, mutatePosts, myPosts, posts, router],
   )
 
   return { createPost }
@@ -41,7 +51,7 @@ export const useUpdatePost = (post: Post) => {
     async (params: PostRequestParams) => {
       try {
         const res = await putApi<Post>(`/posts/${post.id}`, params)
-        if (!res || !posts) {
+        if (!res || !posts || !myPosts) {
           return
         }
         const newPosts = posts.map((post) => {
@@ -50,10 +60,14 @@ export const useUpdatePost = (post: Post) => {
           }
           return post
         })
-
         const newMyPosts = {
-          user: myPosts?.user,
-          posts: newPosts,
+          user: myPosts.user,
+          posts: myPosts.posts.map((post) => {
+            if (post.id === res.id) {
+              return res
+            }
+            return post
+          }),
         }
 
         mutatePosts(newPosts, false)
@@ -65,23 +79,34 @@ export const useUpdatePost = (post: Post) => {
         }
       }
     },
-    [mutatePosts, post.id, posts],
+    [mutateMyPosts, mutatePosts, myPosts, post.id, posts],
   )
 
   return { updatePost }
 }
 
 export const useDeletePost = (post: Post) => {
-  const { data: posts, mutate } = useGetApi<Post[]>('/posts')
+  const { data: user } = useGetApi<User>('/users/me')
+  const { data: posts, mutate: mutatePosts } = useGetApi<Post[]>('/posts')
+  const { data: myPosts, mutate: mutateMyPosts } = useGetApi<MyPosts>(
+    `/users/${user?.id}`,
+  )
 
   const deletePost = useCallback(async () => {
     try {
       const res = await deleteApi(`/posts/${post.id}`)
-      if (!posts) {
+      if (!posts || !myPosts) {
         return
       }
       const newPosts = posts.filter((v) => v.id !== post.id)
-      mutate(newPosts, false)
+
+      const newMyPosts = {
+        user: myPosts.user,
+        posts: myPosts.posts.filter((v) => v.id !== post.id),
+      }
+
+      mutatePosts(newPosts, false)
+      mutateMyPosts(newMyPosts, false)
 
       console.log(res)
     } catch (e) {
@@ -89,7 +114,7 @@ export const useDeletePost = (post: Post) => {
         console.error(e.message)
       }
     }
-  }, [mutate, post.id, posts])
+  }, [mutateMyPosts, mutatePosts, myPosts, post.id, posts])
 
   return { deletePost }
 }
