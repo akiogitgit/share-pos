7c635e5f9cbc028d42bf4af2a0535d6c0499e1fb
akiogitgit <akimaron2828@icloud.com>
2022-10-22 11:11:51 +0900

    feat: フォルダの削除の確認メッセージを作成

--- src/components/bookmark/BookmarkFolderList.tsx
+++ src/components/bookmark/BookmarkFolderList.tsx
@@ -21,6 +21,7 @@ export const BookmarkFolderList: FC<Props> = ({
   // const [editingFolderIndex, setEditingFolderIndex] = useState(-1)
   const [editFolderName, setEditFolderName] = useState('')
   const [isOpenModal, setIsOpenModal] = useState(false)
+  const [isShowDeleteMessage, setIsShowDeleteMessage] = useState(false)
 
   const { updateFolder } = useUpdateFolder()
   const { deleteFolder } = useDeleteFolder()
@@ -53,27 +54,23 @@ export const BookmarkFolderList: FC<Props> = ({
                   : 'text-gray-500 border-b-2'
               }`}
             >
-              <div
-                className={`flex items-center mt-2 ${
+              <button
+                className={`mt-2 w-full flex items-center ${
                   selectedFolderId === folder.id && 'font-bold'
                 }`}
+                onClick={() => onClickFolder(folder.id, folder.name)}
               >
-                <button
-                  className='text-left w-full'
-                  onClick={() => onClickFolder(folder.id, folder.name)}
-                >
-                  <BsFolderIcon className='mr-1' />
-                  {folder.name}
-                </button>
-              </div>
+                <BsFolderIcon className='mr-1' />
+                {folder.name}
+              </button>
             </div>
           ))}
         </div>
       </div>
       <div
         className={`${
-          !isOpenModal && 'hidden'
-        } bg-black h-screen w-screen opacity-20 top-0 left-0 z-100 fixed`}
+          !isOpenModal && !isShowDeleteMessage && 'hidden'
+        } bg-black h-screen w-screen opacity-50 top-0 left-0 z-100 fixed`}
       ></div>
 
       <Dialog
@@ -85,49 +82,87 @@ export const BookmarkFolderList: FC<Props> = ({
           <Dialog.Title className='text-center pt-4'>
             フォルダを編集
           </Dialog.Title>
-          <div className='mx-2 text-center'>
-            <form
-              className='flex justify-center'
-              onSubmit={async (e) => {
-                e.preventDefault()
-                console.log(`update: `, selectedFolderId, editFolderName)
-                await updateFolder(selectedFolderId, editFolderName)
-              }}
-            >
+          <form
+            onSubmit={async (e) => {
+              e.preventDefault()
+              console.log(`update: `, selectedFolderId, editFolderName)
+              setIsOpenModal(false)
+              await updateFolder(selectedFolderId, editFolderName)
+            }}
+          >
+            <div className='flex justify-center'>
               <input
                 type='text'
-                className='border outline-none text-black ring-blue-500 w-100px duration-150 focus:ring-1'
+                className='border outline-none rounded-10px text-black ring-gray-300 w-170px duration-150 focus:ring-1'
                 maxLength={15}
                 value={editFolderName}
                 onChange={(e) => setEditFolderName(e.target.value)}
               />
-              <button
+              {/* <button
                 className='font-bold bg-blue-500 text-white py-0.5 px-1'
                 type='submit'
               >
                 更新
-              </button>
+              </button> */}
               <div
-                className='cursor-pointer font-bold bg-red-500 text-white ml-1 py-0.5 px-1'
+                className='cursor-pointer font-bold bg-red-500 rounded-10px text-white ml-1 py-0.5 px-2 px-1'
                 onClick={async () => {
                   setIsOpenModal(false)
-                  await deleteFolder(selectedFolderId)
+                  setIsShowDeleteMessage(true)
+                  // await deleteFolder(selectedFolderId)
                 }}
               >
-                削除
+                ✕
               </div>
-            </form>
-          </div>
+            </div>
+
+            <div className='flex justify-between'>
+              <button
+                onClick={() => setIsOpenModal(false)}
+                className='border-r-2 border-t-2 mt-4 w-full py-2 text-blue-500'
+              >
+                閉じる
+              </button>
+              <button
+                type='submit'
+                // onClick={() => setIsOpenModal(false)}
+                className='font-bold border-t-2 mt-4 w-full py-2 text-blue-500'
+              >
+                更新
+              </button>
+            </div>
+          </form>
+        </Dialog.Panel>
+      </Dialog>
+
+      <Dialog
+        open={isShowDeleteMessage}
+        onClose={() => setIsShowDeleteMessage(false)}
+        className='m-auto transform top-[50%] left-[50%] z-100 translate-x-[-50%] translate-y-[-50%] fixed'
+      >
+        <Dialog.Panel className='bg-white bg-opacity-90 rounded-10px shadow-2xl w-300px'>
+          <Dialog.Title className='text-center px-2 pt-4'>
+            {editFolderName}を削除しますか？
+          </Dialog.Title>
+          <p className='text-center px-2'>
+            このフォルダの記事が全て削除されます。
+            <br /> この操作は取り消せません
+          </p>
+
           <div className='flex justify-between'>
             <button
-              onClick={() => setIsOpenModal(false)}
-              className='font-bold border-r-2 border-t-2 mt-4 w-full py-2 text-blue-500'
+              onClick={() => setIsShowDeleteMessage(false)}
+              className='border-r-2 border-t-2 mt-4 w-full py-2 text-blue-500'
             >
               閉じる
             </button>
             <button
-              onClick={() => setIsOpenModal(false)}
-              className='font-bold border-t-2 mt-4 w-full py-2 text-blue-500'
+              type='submit'
+              onClick={async () => {
+                setIsShowDeleteMessage(false)
+                await deleteFolder(selectedFolderId)
+              }}
+              className='font-bold border-t-2 mt-4 w-full py-2 text-red-500'
             >
               削除
             </button>
