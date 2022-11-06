e3b0d62862e9dd1371f68db4f35670c0deed7e49
akiogitgit <akimaron2828@icloud.com>
2022-11-06 17:58:17 +0900

    feat: variant

--- src/components/shares/Button.tsx
+++ src/components/shares/Button.tsx
@@ -8,6 +8,7 @@ type Props = {
   size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
   radius?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
   animate?: boolean
+  variant?: 'outline' | 'light' | 'filled'
   fullWidth?: boolean
   rightIcon?: React.ReactElement
   leftIcon?: React.ReactElement
@@ -21,6 +22,7 @@ export const Button: FC<Props> = ({
   color = 'red',
   size = 'sm',
   radius = 'sm',
+  variant = 'filled',
   animate,
   fullWidth,
   rightIcon,
@@ -28,24 +30,114 @@ export const Button: FC<Props> = ({
   onClick,
 }) => {
   const defaultClass =
-    'font-bold flex items-center justify-center gap-1 hover:bg-opacity-80'
+    'duration-300 font-bold flex items-center justify-center gap-1 '
 
-  const colorClass = `bg-${color}-500 text-white border border-${color}-500`
+  // const bg50 = useMemo(() => {
+  //   if (color === 'red') return 'bg-red-50'
+  //   if (color === 'blue') return 'bg-blue-50'
+  //   if (color === 'gray') return 'bg-gray-50'
+  // }, [color])
+  // const bg100 = useMemo(() => {
+  //   if (color === 'red') return 'bg-red-100'
+  //   if (color === 'blue') return 'bg-blue-100'
+  //   if (color === 'gray') return 'bg-gray-100'
+  // }, [color])
+  // const bg500 = useMemo(() => {
+  //   if (color === 'red') return 'bg-red-500'
+  //   if (color === 'blue') return 'bg-blue-500'
+  //   if (color === 'gray') return 'bg-gray-500'
+  // }, [color])
+  // const text500 = useMemo(() => {
+  //   if (color === 'red') return 'text-red-500'
+  //   if (color === 'blue') return 'text-blue-500'
+  //   if (color === 'gray') return 'text-gray-500'
+  // }, [color])
+  // const border500 = useMemo(() => {
+  //   if (color === 'red') return 'border-red-500'
+  //   if (color === 'blue') return 'border-blue-500'
+  //   if (color === 'gray') return 'border-gray-500'
+  // }, [color])
 
-  // const colorClass = (() => {
+  // const hoverbg50 = useMemo(() => {
+  //   if (color === 'red') return 'hover:bg-red-50'
+  //   if (color === 'blue') return 'hover:bg-blue-50'
+  //   if (color === 'gray') return 'hover:bg-gray-50'
+  // }, [color])
+  // const hoverbg100 = useMemo(() => {
+  //   if (color === 'red') return 'hover:bg-red-100'
+  //   if (color === 'blue') return 'hover:bg-blue-100'
+  //   if (color === 'gray') return 'hover:bg-gray-100'
+  // }, [color])
+  // const hoverbg500 = useMemo(() => {
+  //   if (color === 'red') return 'hover:bg-red-500'
+  //   if (color === 'blue') return 'hover:bg-blue-500'
+  //   if (color === 'gray') return 'hover:bg-gray-500'
+  // }, [color])
+  // const hovertext500 = useMemo(() => {
+  //   if (color === 'red') return 'hover:text-red-500'
+  //   if (color === 'blue') return 'hover:text-blue-500'
+  //   if (color === 'gray') return 'hover:text-gray-500'
+  // }, [color])
+  // const hoverborder500 = useMemo(() => {
+  //   if (color === 'red') return 'hover:border-red-500'
+  //   if (color === 'blue') return 'hover:border-blue-500'
+  //   if (color === 'gray') return 'hover:border-gray-500'
+  // }, [color])
+
+  const colorFilled = `bg-${color}-500 text-white border-1 border-${color}-500`
+  const colorLight = `bg-${color}-50 text-${color}-500 border-1 border-${color}-50`
+  const colorOutline = `bg-white text-${color}-500 border-1 border-${color}-500`
+  // const colorFilled = `${bg500} text-white border-1 border-${color}-500`
+  // const colorLight = `${bg50} text-${color}-500 border-1 border-${color}-50`
+  // const colorOutline = `bg-white text-${color}-500 border-1 border-${color}-500`
+
+  // const colorFilled = useMemo(() => {
+  //   const common = 'text-white border-1'
   //   switch (color) {
   //     case 'red':
-  //       return 'bg-red-500 text-white'
+  //       return `${common} bg-red-500 border-red-500`
   //     case 'blue':
-  //       return 'bg-blue-500 text-white'
+  //       return `${common} bg-blue-500 border-blue-500`
   //     case 'gray':
-  //       return 'bg-gray-500 text-white'
+  //       return `${common} bg-gray-500 border-gray-500`
   //   }
-  // })()
+  // }, [color])
 
-  const animateClass = animate
-    ? `duration-300 hover:text-${color}-500 hover:bg-white`
-    : ''
+  // const colorLight = `bg-${color}-50 text-${color}-500 border-1 border-${color}-50`
+  // const colorOutline = `bg-white text-${color}-500 border-1 border-${color}-500`
+
+  const colorClass = (() => {
+    switch (variant) {
+      case 'filled':
+        return colorFilled
+      case 'light':
+        return colorLight
+      case 'outline':
+        return colorOutline
+    }
+  })()
+
+  const animateClass = (() => {
+    if (!animate) {
+      switch (variant) {
+        case 'filled':
+          return `hover:bg-opacity-80`
+        case 'light':
+          return `hover:bg-${color}-100`
+        case 'outline':
+          return `hover:bg-${color}-50`
+      }
+    }
+
+    switch (variant) {
+      case 'filled':
+        return `hover:text-${color}-500 hover:bg-white`
+      case 'light':
+        return `hover:bg-${color}-500 hover:text-white`
+      case 'outline':
+        return `hover:bg-${color}-500 hover:text-white`
+    }
+  })()
 
   const sizeClass = (() => {
     switch (size) {
@@ -80,8 +172,6 @@ export const Button: FC<Props> = ({
   const allClass = `${defaultClass} ${className} ${colorClass} ${animateClass} ${sizeClass} ${RadiusClass} ${fullWidthClass}`
 
   return (
-    // <button className='bg-primary border border-primary flex rounded-7px text-white py-1 px-2 gap-1 duration-300 items-center hover:(bg-white text-primary) '>
-    // <button className={`${[colorClass, sizeClass]}`}>
     <button className={allClass} onClick={onClick} type={type}>
       {leftIcon}
       {children}
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -8,15 +8,62 @@ const Test: NextPage = () => {
       <div className='flex gap-2 items-start'>
         <Button size='xs'>Settings</Button>
         {/* <div className='w-200px'>
-          <Button size='sm' fullWidth onClick={() => alert('a')}>
-            Settings Settings
-          </Button>
-        </div> */}
+        <Button size='sm' fullWidth onClick={() => alert('a')}>
+          Settings Settings
+        </Button>
+      </div> */}
         <Button size='sm'>Settings</Button>
-        <Button size='md'>Settings</Button>
-        <Button size='lg'>Settings</Button>
+        <Button size='md' color='gray'>
+          Settings
+        </Button>
+        <Button size='lg' color='blue'>
+          Settings
+        </Button>
         <Button size='xl' radius='sm'>
           Settings
+        </Button>
+      </div>
+
+      <div className='flex gap-2 items-start'>
+        <Button color='blue'>filled</Button>
+        <Button color='blue' variant='light'>
+          light
+        </Button>
+        <Button color='blue' variant='outline'>
+          outline
+        </Button>
+      </div>
+      <div className='flex mt-4 gap-2 items-start'>
+        <Button color='blue' animate>
+          filled
+        </Button>
+        <Button color='blue' animate variant='light'>
+          light
+        </Button>
+        <Button color='blue' animate variant='outline'>
+          outline
+        </Button>
+      </div>
+      <div className='flex mt-4 gap-2 items-start'>
+        <Button color='red' animate>
+          filled
+        </Button>
+        <Button color='red' animate variant='light'>
+          light
+        </Button>
+        <Button color='red' animate variant='outline'>
+          outline
+        </Button>
+      </div>
+      <div className='flex mt-4 gap-2 items-start'>
+        <Button color='gray' animate>
+          filled
+        </Button>
+        <Button color='gray' animate variant='light'>
+          light
+        </Button>
+        <Button color='gray' animate variant='outline'>
+          outline
         </Button>
       </div>
 
