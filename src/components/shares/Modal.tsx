b87d2a15368e178c27ad81822a5d51263a366006
akiogitgit <akimaron2828@icloud.com>
2022-11-19 09:19:55 +0900

    fix: PostLinkCardのrounded修正

--- src/components/shares/Modal.tsx
+++ src/components/shares/Modal.tsx
@@ -20,7 +20,7 @@ export const Modal: FC<Props> = ({ open, onClose, title, children }) => {
             />
 
             <Dialog.Panel className='transform top-[50%] left-[50%] z-2 translate-x-[-50%] translate-y-[-50%] fixed'>
-              <div className='bg-white bg-opacity-90 rounded-10px shadow-2xl w-300px '>
+              <div className='bg-white bg-opacity-97 rounded-10px shadow-2xl w-300px '>
                 <Dialog.Title className='font-bold text-center text-xl px-2 pt-4'>
                   {title}
                 </Dialog.Title>
