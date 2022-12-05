792094763b939269eeb69dfc48e28b7b0c74327f
akiogitgit <akimaron2828@icloud.com>
2022-12-05 23:56:49 +0900

    feat: ヘッダーのフォントをcantore oneにする

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,3 @@
-import { Lora, Inter, Kenia, Dancing_Script } from '@next/font/google'
 import { FC, ReactNode } from 'react'
 import { Footer } from './Footer'
 import { Header } from './header/Header'
@@ -6,34 +5,17 @@ import { Header } from './header/Header'
 type Props = {
   children: ReactNode
 }
-const lora = Lora({
-  variable: '--font-lora',
-})
-const inter = Inter({
-  variable: '--font-inter',
-})
-const dancing = Dancing_Script({
-  variable: '--font-dancing',
-})
-const kenia = Kenia({
-  weight: '400',
-  variable: '--font-kenia',
-})
 
 export const Layout: FC<Props> = ({ children }) => {
   return (
-    <div
-      className={`${inter.className} ${lora.className} ${dancing.className} ${kenia.className}`}
-    >
-      <div className='bg-base min-h-screen'>
-        <Header />
-        <main>
-          <div className='mx-auto max-w-1150px px-4 pt-50px pb-150px'>
-            {children}
-          </div>
-        </main>
-        <Footer />
-      </div>
+    <div className='bg-base min-h-screen'>
+      <Header />
+      <main>
+        <div className='mx-auto max-w-1150px px-4 pt-50px pb-150px'>
+          {children}
+        </div>
+      </main>
+      <Footer />
     </div>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,52 +1,22 @@
-import { Lora, Inter, Kenia, Dancing_Script } from '@next/font/google'
+import { Cantora_One } from '@next/font/google'
 import type { AppProps } from 'next/app'
 import { CookiesProvider } from 'react-cookie'
 import { SWRConfig } from 'swr'
 // eslint-disable-next-line import/no-unresolved
 import 'windi.css'
-// const lora = Lora({
-//   subsets: ['latin'], // 全てのfont-familyに適応
-// })
-// const lora = Lora()
-// const inter = Inter()
-// const dancing = Dancing_Script()
-// const kenia = Kenia({ weight: '400' })
 
-const lora = Lora({
-  variable: '--font-lora',
-})
-const inter = Inter({
-  variable: '--font-inter',
-})
-const dancing = Dancing_Script({
-  variable: '--font-dancing',
-})
-const kenia = Kenia({
-  weight: '400',
-  variable: '--font-kenia',
-})
+const cantoreOne = Cantora_One({ weight: '400' })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SWRConfig>
       <CookiesProvider>
-        <div
-          className={`${inter.className} ${lora.className} ${dancing.className} ${kenia.className}`}
-        >
-          <Component {...pageProps} />
-        </div>
+        <Component {...pageProps} />
 
         <style jsx global>
           {`
             :root {
-              --font-lora: ${lora.style.fontFamily};
-              --font-kenia: ${kenia.style.fontFamily};
-              --font-dancing: ${dancing.style.fontFamily};
-            }
-             {
-              /* html {
-              font-family: ${dancing.style.fontFamily};
-            } */
+              --font-cantoreOne: ${cantoreOne.style.fontFamily};
             }
           `}
         </style>
