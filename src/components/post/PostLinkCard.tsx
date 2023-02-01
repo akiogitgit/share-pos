9a98eac52889934f98c21353e81c09dedacb4edd
akiogitgit <akimaron2828@icloud.com>
2023-02-01 17:23:23 +0900

    feat: Postのmeta情報がない時のエラー処理

--- src/components/post/PostLinkCard.tsx
+++ src/components/post/PostLinkCard.tsx
@@ -9,13 +9,13 @@ type Props = {
 export const PostLinkCard: FC<Props> = ({ post }) => {
   // ドメインによって、表示するURLを変える
   const displayURL = useMemo(() => {
-    if (!post.metaInfo.image) return ''
+    if (!post.metaInfo?.image) return ''
     return ['qiita-user', 'res.cloudinary', 'data:image/png;base64'].some(v =>
       post.metaInfo.image?.includes(v),
     )
       ? post.metaInfo.image
       : `https://res.cloudinary.com/demo/image/fetch/${post.metaInfo.image}`
-  }, [post.metaInfo.image])
+  }, [post.metaInfo?.image])
 
   return (
     <>
@@ -32,7 +32,7 @@ export const PostLinkCard: FC<Props> = ({ post }) => {
                 objectFit='contain'
               />
             ) : (
-              <div className='flex h-full bg-gray-300 text-mono w-full max-h-225px transform text-30px duration-300 overflow-hidden items-center justify-center group-hover:scale-110'>
+              <div className='flex h-full bg-gray-300 text-mono w-full max-h-225px transform text-2xl duration-300 overflow-hidden items-center justify-center group-hover:scale-110'>
                 No image
               </div>
             )}
@@ -43,7 +43,7 @@ export const PostLinkCard: FC<Props> = ({ post }) => {
               {post.url.split('//')[1].split('/')[0]}
             </p>
             <div className='h-37px mt-2 text-sm overflow-hidden'>
-              {post.metaInfo.title}
+              {post.metaInfo?.title}
             </div>
           </figcaption>
         </a>
