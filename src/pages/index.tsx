36383b87bfe24e296d2353f787ee8a6a555057ea
akiogitgit <akimaron2828@icloud.com>
2023-02-04 10:16:49 +0900

    feat: #99 useSWRInfiniteで、postsのローディングを実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,39 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
 
-import { useState } from 'react'
+import { useCallback } from 'react'
+import useSWRInfinite from 'swr/infinite'
 import { Layout } from 'components/layout/Layout'
 import { PostItem } from 'components/post/PostItem'
 import { Button } from 'components/shares/base/Button'
-import { useGetApi } from 'hooks/useApi'
 import { Post } from 'types/post'
-
-const totalPosts: Post[] = []
-let isSetPosts = false
+import { fetchApi } from 'utils/api'
 
 const Home: NextPage = () => {
-  const [page, setPage] = useState(1)
-  const { data: posts, error } = useGetApi<Post[]>(`/posts?page=${page}`)
-
-  if (posts?.length && !isSetPosts) {
-    totalPosts.push(...posts)
-    isSetPosts = true
+  const getKey = (pageIndex: number, previousPageData: Post[][]) => {
+    if (previousPageData && !previousPageData.length) return null // 最後に到達した
+    return `/posts?page=${pageIndex + 1}` // SWR キー
   }
+
+  const fetcher = useCallback(
+    async (url: string) => await fetchApi<Post[]>(url, 'GET'),
+    [],
+  )
+
+  const {
+    data: posts,
+    size: page,
+    setSize: setPage,
+  } = useSWRInfinite(getKey, fetcher, {
+    revalidateOnReconnect: false,
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+  })
+
+  const limit = 24
+  const isEmpty = posts?.[0]?.length === 0
+  const isReachingEnd =
+    isEmpty || (posts && posts?.[posts?.length - 1]?.length < limit)
 
   return (
     <>
@@ -26,10 +41,10 @@ const Home: NextPage = () => {
         <title>SharePos 投稿一覧ページ</title>
       </Head>
       <Layout>
-        {totalPosts && (
+        {posts && (
           <div className='mt-4'>
             <div className='grid gap-6 justify-center items-start sm:(grid-cols-[repeat(auto-fill,minmax(291px,auto))])'>
-              {totalPosts.map((post, i) => (
+              {posts.flat().map((post, i) => (
                 <div key={i} className='mb-1'>
                   <PostItem post={post} />
                 </div>
@@ -38,20 +53,19 @@ const Home: NextPage = () => {
           </div>
         )}
 
-        <div className='mt-10 text-center'>
-          {posts?.length === 24 && (
+        {!isReachingEnd && (
+          <div className='mt-10 text-center'>
             <Button
               variant='neumorphism'
               size='lg'
               onClick={() => {
                 setPage(page + 1)
-                isSetPosts = false
               }}
             >
               もっと見る
             </Button>
-          )}
-        </div>
+          </div>
+        )}
       </Layout>
     </>
   )
