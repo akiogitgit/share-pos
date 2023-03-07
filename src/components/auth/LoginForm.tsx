42e4c45f7d31ec76c10e1701eea22e00a77e936e
akiogitgit <akimaron2828@icloud.com>
2023-03-07 10:42:58 +0900

    feat: ログインフォームのバリデーション実装

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,56 +1,64 @@
-import { FC, useState } from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { FC } from 'react'
 
+import { useForm } from 'react-hook-form'
 import { AiOutlineMail as AiOutlineMailIcon } from 'react-icons/ai'
 import { RiLockPasswordLine as RiLockPasswordLineIcon } from 'react-icons/ri'
+import { z } from 'zod'
 import { Button } from 'components/shares/base/Button'
-
-import { LoginRequestParams } from 'types/auth'
 
 type Props = {
   onSubmit: (params: LoginRequestParams) => void
 }
 
+const schema = z.object({
+  email: z.string().email('メールの形式で入力して下さい'),
+  password: z.string().min(6, { message: '6文字以上で入力して下さい' }),
+})
+
+export type LoginRequestParams = z.infer<typeof schema>
+
 export const LoginForm: FC<Props> = ({ onSubmit }) => {
-  const [email, setEmail] = useState<string>('test1@test.com')
-  const [password, setPassword] = useState<string>('password')
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginRequestParams>({
+    resolver: zodResolver(schema),
+  })
 
   return (
     <div className='flex flex-col items-center justify-center'>
-      <form
-        onSubmit={e => {
-          onSubmit({ email, password })
-          e.preventDefault()
-        }}
-      >
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className='max-w-300px w-80vw relative'>
           <label>
             Eメール
             <input
               type='text'
-              value={email}
-              placeholder='example@example.com'
-              required
-              onChange={v => setEmail(v.target.value)}
               className='border outline-none ring-primary-dark w-full p-2 pr-9 duration-300 focus:rounded-md focus:ring-1'
+              {...register('email')}
             />
           </label>
           <AiOutlineMailIcon className='top-37px left-270px absolute' />
         </div>
+        {errors?.email && (
+          <div className='text-danger-dark'>{errors?.email.message}</div>
+        )}
+
         <div className='mt-2 relative '>
           <label>
             パスワード
             <input
               type='password'
-              value={password}
-              placeholder='password'
-              required
-              onChange={v => setPassword(v.target.value)}
               className='border outline-none ring-primary-dark w-full p-2 pr-9 duration-300 focus:rounded-md focus:ring-1'
+              {...register('password')}
             />
           </label>
           <RiLockPasswordLineIcon className='top-37px left-270px absolute' />
+          {errors?.password && (
+            <div className='text-danger-dark'>{errors?.password.message}</div>
+          )}
         </div>
-
         <Button type='submit' fullWidth animate className='mt-4'>
           ログイン
         </Button>
